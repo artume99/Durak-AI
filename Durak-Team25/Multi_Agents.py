520925177392adebd7150cd799144c0bc16da7b6
yam fine <yamf45@gmail.com>
2022-08-02 22:32:31 +0300

    bruh

--- Durak-Team25/Multi_Agents.py
+++ Durak-Team25/Multi_Agents.py
@@ -1,23 +1,8 @@
-from .Deck import hand_out_cards
+# from .Deck import hand_out_cards
 from .Game import Agent
 
 
-class Player(Agent):
-    def __init__(self, deck):
-        super().__init__()
-        self.deck = deck
-        self.hand = deck.hand_out_cards(6)
-
-    def get_action(self, game_state):
-        """
-        calculate the appropriate action
-        :param game_state:
-        :return:
-        """
-        pass
-
-
-class Algo1(Player):
+class Algo1(Agent):
     def __init__(self, deck):
         super().__init__(deck)
 
